90f1f34462c61d48df0aa143a9a7134bb79c076d
tournwin <tournwin@gmail.com>
2018-08-02 00:23:43 +0300

    hw03

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -65,9 +65,19 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+	strTickNum = str(ticket_number)
+	if (len(strTickNum) == 6):
+		mas = list(strTickNum)
+		sum1 = int(mas[0]) + int(mas[1]) + int(mas[2])
+		sum2 = int(mas[3]) + int(mas[4]) + int(mas[5])
+		if (sum1 == sum2):
+			return 'Ваш билет - Счастливый! Поздравляю!'
+		else:
+			return 'Не сегодня. Попробуйте завтра!'
+	else:
+		return 'Код билета введён не корректно'
 
 
-#print(lucky_ticket(123006))
-#print(lucky_ticket(12321))
-#print(lucky_ticket(436751))
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
