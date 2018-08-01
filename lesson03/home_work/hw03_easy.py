c5a5402ddfe645160259a00f4bcaa8cc451d6be2
tournwin <tournwin@gmail.com>
2018-08-01 23:50:05 +0300

    hw03

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,12 +5,41 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    paste
+	strNumber = str(number)
+	dotPos = strNumber.find('.')
+	if (dotPos != -1): # Если есть точка в строке
+		strBeforeDot = strNumber[:dotPos] # Строка до точки
+		strAfterDot = strNumber[dotPos+1::] # строка после точки
+		if (ndigits > 0):
+			if (len(strAfterDot) != ndigits):
+				if (len(strAfterDot) > ndigits):
+					# Если номер ndigits элемента строки > 5,
+					# то +1 предыдущему символу
+					# Иначе -1
+					afterLastNum = int(strAfterDot[ndigits])
+					strRoundNum = strAfterDot[:ndigits]
+					lastNum = int(strRoundNum[-1])
+					if (afterLastNum >= 5):
+						strRoundNum = strRoundNum[:-1] + str(lastNum+1)
+					else:
+						strRoundNum = strRoundNum[:-1] + str(lastNum-1)
+					return float(strNumber[:dotPos+1]+strRoundNum)
+				else:
+					### Дописать нулями
+					#while len(strAfterDot) != ndigits:
+					#	strAfterDot += "0"
+					#return float(strNumber[:dotPos+1]+strAfterDot)
+					return number
+			else:
+				return number
+		else:
+			return int(strBeforeDot)
+	else:
+		return number
 
-
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
+print(my_round(2.1234567, 0))
+#print(my_round(2.1999967, 5))
+#print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -23,6 +52,6 @@ def lucky_ticket(ticket_number):
     pass
 
 
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+#print(lucky_ticket(123006))
+#print(lucky_ticket(12321))
+#print(lucky_ticket(436751))
